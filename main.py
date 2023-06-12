e7eb49f862484c1b40fe194651e8586169478be3
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-13 05:19:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from langchain.prompts.chat import (
     HumanMessagePromptTemplate,
 )
 from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationTokenBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import ConversationChain
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.agents import load_tools
