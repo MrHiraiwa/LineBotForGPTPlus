fb05d6b14065390d04d0bb9efb14e5d3d1d499bb
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-14 00:54:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, abort, render_template, session
+from flask import Flask, request, abort, render_template, session, redirect
 from google.cloud import firestore
 from googleapiclient.discovery import build
 from linebot import (
