15883ccab070a19d54dff17a9b3498d8cc15bb66
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-13 22:00:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,10 +27,6 @@ app = Flask(__name__)
 # LINE Messaging APIの準備
 line_bot_api = LineBotApi(os.environ["CHANNEL_ACCESS_TOKEN"])
 handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
-
-# Firestore setup
-db = firestore.Client()
-
 # 設定プロンプト
 character_setting = "私は有能な秘書です。"
 # チャットプロンプトテンプレート
@@ -39,7 +35,6 @@ prompt = ChatPromptTemplate.from_messages([
     MessagesPlaceholder(variable_name="history"),
     HumanMessagePromptTemplate.from_template("{input}")
 ])
-
 # チャットモデル
 llm = ChatOpenAI(
     model_name="gpt-3.5-turbo",
@@ -47,49 +42,32 @@ llm = ChatOpenAI(
     temperature=1,
     streaming=True
 )
-
 # メモリ
-memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=200, return_messages=True)
-
+#memory = ConversationBufferWindowMemory(k=3, return_messages=True)
+#memory = ConversationTokenBufferMemory(llm=llm, max_token_limit=2000, return_messages=True)
+memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=2000, return_messages=True)
 # 会話チェーン
 conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm, verbose=True)
-
 @app.route("/", methods=["POST"])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers["X-Line-Signature"]
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
     return "OK"
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     response = conversation.predict(input=event.message.text)
-
-    # Save conversation to Firestore
-    user_id = event.source.user_id
-    doc_ref = db.collection('conversations').document(user_id)
-    doc_ref.set({
-        'conversation': firestore.ArrayUnion([{
-            'user_message': event.message.text,
-            'bot_response': response
-        }])
-    }, merge=True)
-
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=response)
     )
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
