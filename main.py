7277b08cc8729acbc487291ab9652bcd9302ae47
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-13 22:07:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferWindowMemory, ConversationTokenBufferMemory, ConversationSummaryBufferMemory
 from langchain.chains import ConversationChain
 import tiktoken
+import pickle
 
 app = Flask(__name__)
 
@@ -44,10 +45,40 @@ llm = ChatOpenAI(
 )
 # メモリ
 #memory = ConversationBufferWindowMemory(k=3, return_messages=True)
-#memory = ConversationTokenBufferMemory(llm=llm, max_token_limit=2000, return_messages=True)
-memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=2000, return_messages=True)
+# memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=2000, return_messages=True)
+memory = CustomConversationSummaryBufferMemory(llm=llm, max_token_limit=2000, return_messages=True)
+
 # 会話チェーン
 conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm, verbose=True)
+
+db = firestore.Client()
+
+class CustomConversationSummaryBufferMemory(ConversationSummaryBufferMemory):
+    def get_state(self):
+        return self.__dict__
+
+    def set_state(self, state):
+        self.__dict__.update(state)
+
+
+
+def get_user_memory(user_id):
+    doc_ref = db.collection('memory').document(user_id)
+    doc = doc_ref.get()
+    if doc.exists:
+        memory_state = pickle.loads(doc.to_dict()['memory'])
+        return memory_state
+    else:
+        return None
+
+def save_user_memory(user_id, memory):
+    doc_ref = db.collection('memory').document(user_id)
+    memory_state = pickle.dumps(memory)
+    doc_ref.set({
+        'memory': memory_state
+    })
+
+    
 @app.route("/", methods=["POST"])
 def callback():
     # get X-Line-Signature header value
@@ -62,12 +93,26 @@ def callback():
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
     return "OK"
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    user_id = event.source.user_id
+
+    # Get memory state from Firestore
+    memory_state = get_user_memory(user_id)
+    if memory_state is not None:
+        memory.set_state(memory_state)
+
     response = conversation.predict(input=event.message.text)
+
+    # Save memory state to Firestore
+    memory_state = memory.get_state()
+    save_user_memory(user_id, memory_state)
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=response)
     )
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
