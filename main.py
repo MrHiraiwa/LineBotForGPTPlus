12fbfcdbca8b42cfaace85782251876bd74e43f9
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-13 20:41:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,8 @@ llm = ChatOpenAI(
 )
 # メモリ
 #memory = ConversationBufferWindowMemory(k=3, return_messages=True)
-memory = ConversationTokenBufferMemory(llm=llm, max_token_limit=2000, return_messages=True)
+#memory = ConversationTokenBufferMemory(llm=llm, max_token_limit=2000, return_messages=True)
+memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=2000, return_messages=True)
 # 会話チェーン
 conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm, verbose=True)
 @app.route("/", methods=["POST"])
