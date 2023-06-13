8361920f322fe66e80d58036277f1b71da0f974f
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-13 18:57:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, request, abort
 from google.cloud import firestore
-from googleapiclient.discovery import build
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -26,6 +25,8 @@ import tiktoken
 
 app = Flask(__name__)
 
+# Firestoreの準備
+db = firestore.Client()
 
 # LINE Messaging APIの準備
 line_bot_api = LineBotApi(os.environ["CHANNEL_ACCESS_TOKEN"])
@@ -40,6 +41,7 @@ prompt = ChatPromptTemplate.from_messages([
     MessagesPlaceholder(variable_name="history"),
     HumanMessagePromptTemplate.from_template("{input}")
 ])
+
 # チャットモデル
 llm = ChatOpenAI(
     model_name="gpt-3.5-turbo",
@@ -49,9 +51,11 @@ llm = ChatOpenAI(
 )
 
 tools = load_tools(["google-search"], llm=llm)
+
 # メモリ
 #memory = ConversationBufferWindowMemory(k=3, return_messages=True)
 memory = ConversationTokenBufferMemory(llm=llm, max_token_limit=2000, return_messages=True)
+
 # 会話チェーン
 conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm, verbose=True)
 
@@ -75,7 +79,25 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    user_id = event.source.user_id
+
+    # ユーザーIDを基にFirestoreからドキュメントを取得
+    doc_ref = db.collection('users').document(user_id)
+    doc = doc_ref.get()
+
+    # ドキュメントが存在すれば、会話の履歴をメモリに読み込む
+    if doc.exists:
+        memory.set_memory(doc.to_dict()["history"])
+    else:
+        # ドキュメントが存在しなければ、新たに作成する
+        doc_ref.set({"history": []})
+
+    # メッセージを処理して応答を生成
     response = conversation.predict(input=event.message.text)
+
+    # メモリの内容をFirestoreに保存
+    doc_ref.update({"history": memory.get_memory()})
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=response)
