96e785a0c626499e1caf721f6f774cd4d78b9f4f
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-15 00:25:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -265,7 +265,7 @@ def handle_message(event):
         return 'Not a valid JSON', 200 
     except Exception as e:
         print(f"Error in lineBot: {e}")
-        callLineApi(ERROR_MESSAGE, replyToken, {'items': quick_reply})
+        line_reply(reply_token, "エラーが発生しました。")
         raise
     finally:
         return 'OK'
