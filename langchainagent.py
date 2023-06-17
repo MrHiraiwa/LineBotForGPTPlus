f2e9d6ab39e6115ee0d492c044d1de7c372f1bc0
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-18 07:27:36 +0900

    Update langchainagent.py

--- langchainagent.py
+++ langchainagent.py
@@ -1,17 +1,25 @@
-from langchain import SerpAPIWrapper
 from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
+from langchain.utilities.google_search import GoogleSearchAPIWrapper
+from langchain import LLMMathChain
 
+google_search = GoogleSearchAPIWrapper()
+llm_math_chain = LLMMathChain(llm=llm, verbose=True)
 
 llm = ChatOpenAI(temperature=0.0, model="gpt-3.5-turbo-0613")
 search = SerpAPIWrapper()
-wolfram = WolframAlphaAPIWrapper()
+
 tools = [
     Tool(
-        name = "Search",
-        func = search.run,
-        description = "useful for when you need to answer questions about current events. You should ask targeted questions"
+        name = "Google Search",
+        func=google_search.run,
+        description="最新の話題について答える場合に利用することができます。また、今日の日付や今日の気温、天気、為替レートなど現在の状況についても確認することができます。入力は検索内容です。"
+    ),
+    Tool(
+        name="Calculator",
+        func=llm_math_chain.run,
+        description="計算をする場合に利用することができます。"
     )
 ]
 
