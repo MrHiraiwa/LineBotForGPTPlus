e5133129dadc09f8d0182f15958e264c6b634d0a
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-18 06:46:16 +0900

    Update langchainagent.py

--- langchainagent.py
+++ langchainagent.py
@@ -9,16 +9,16 @@ llm = ChatOpenAI(temperature=0.0, model="gpt-3.5-turbo-0613")
 search = SerpAPIWrapper()
 wolfram = WolframAlphaAPIWrapper()
 tools = [
-    Tool(
-        name = "Search",
-        func=search.run,
-        description="useful for when you need to answer questions about current events. You should ask targeted questions"
-    ),
-    Tool(
-        name="WolframAlpha",
-        func=wolfram.run,
-        description="useful for when you need to answer questions about math and science"
-    )
+    Tool(
+        name = "Search",
+        func = search.run,
+        description = "useful for when you need to answer questions about current events. You should ask targeted questions"
+    ),
+    Tool(
+        name = "WolframAlpha",
+        func = wolfram.run,
+        description = "useful for when you need to answer questions about math and science"
+    )
 ]
 
 mrkl = initialize_agent(tools, llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=True)
