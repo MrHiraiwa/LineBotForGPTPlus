f297effe4eb1c444801577ad0a3f3d82f8bcc08c
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-20 14:14:05 +0900

    Update langchainagent.py

--- langchainagent.py
+++ langchainagent.py
@@ -1,20 +1,19 @@
 from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
-from langchain.utilities import GoogleSearchAPIWrapper
+from langchain.utilities.google_search import GoogleSearchAPIWrapper
 
 llm = ChatOpenAI(model="gpt-3.5-turbo-0613")
 
-search = GoogleSearchAPIWrapper()
+google_search = GoogleSearchAPIWrapper()
 
 tools = [
     Tool(
         name = "Search",
-        func=search.run,
+        func=google_search.run,
         description="useful for when you need to answer questions about current events. it is single-input tool Search."
     ),
 ]
-
 mrkl = initialize_agent(tools, llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=True)
 
 def langchain_agent(question):
