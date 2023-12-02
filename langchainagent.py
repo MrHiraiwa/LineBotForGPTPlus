28c674abdd03b86c3247feac3a0c9b9e5e80f65c
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-12-03 04:26:48 +0900

    Update langchainagent.py

--- langchainagent.py
+++ langchainagent.py
@@ -3,6 +3,7 @@ from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
 from langchain.utilities.google_search import GoogleSearchAPIWrapper
 import openai
+from datetime import datetime, time, timedelta
 
 llm = ChatOpenAI(model="gpt-3.5-turbo")
 
