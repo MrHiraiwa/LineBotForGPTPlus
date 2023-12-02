7ee84514c18805f3a8655a1ead2db1b4837d4dca
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-12-03 01:20:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ from langchain.prompts.chat import (
 )
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import (
-    CombinedMemory,
     ConversationBufferWindowMemory,
     ConversationTokenBufferMemory,
     ConversationSummaryBufferMemory,
