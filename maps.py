53e0f450666ccef85fb6f6abf63cb14c8004dae7
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-12-04 10:18:20 +0900

    Update maps.py

--- maps.py
+++ maps.py
@@ -1,6 +1,9 @@
 def get_addresses(latitude, longitude, result_type="political"):
-    # result_typeパラメータを使用して特定の住所タイプに絞り込む
-    results = gmaps.reverse_geocode((latitude, longitude), result_type=[result_type] if result_type else None)
+    results = gmaps.reverse_geocode(
+        (latitude, longitude), 
+        result_type=[result_type] if result_type else None,
+        language='ja'
+    )
     addresses = []
     for result in results:
         addr = result['formatted_address']
