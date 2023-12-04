a86b08ac051751e31f79ffd83543a6c05e38d1a4
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-12-04 12:05:46 +0900

    Update maps.py

--- maps.py
+++ maps.py
@@ -1,19 +1,27 @@
 import os
 import googlemaps
+from googlemaps.exceptions import ApiError, HTTPError, Timeout, TransportError
 
 GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
-
 gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
 
 def get_addresses(latitude, longitude, result_type="political"):
-    results = gmaps.reverse_geocode(
-        (latitude, longitude), 
-        result_type=[result_type] if result_type else None,
-        language='ja'
-    )
-    addresses = []
-    for result in results:
-        addr = result['formatted_address']
-        addresses.append(addr)
-    print(f"{addresses}")
-    return addresses
+    try:
+        results = gmaps.reverse_geocode(
+            (latitude, longitude), 
+            result_type=[result_type] if result_type else None,
+            language='ja'
+        )
+        addresses = [result['formatted_address'] for result in results]
+        return addresses
+    except ApiError as e:
+        print(f"API Error: {e}")
+    except HTTPError as e:
+        print(f"HTTP Error: {e}")
+    except Timeout as e:
+        print(f"Timeout Error: {e}")
+    except TransportError as e:
+        print(f"Transport Error: {e}")
+    except Exception as e:
+        print(f"General Error: {e}")
+    return []
