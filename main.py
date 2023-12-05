daf1c6133aa0cb733d0ec62f3f1f6254785dac38
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-12-05 16:49:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -564,6 +564,7 @@ def handle_message(event):
                 user_message = CHANGE_TO_TEXT_MESSAGE
                 transaction.set(doc_ref, {**user, 'messages': [{**msg, 'content': get_encrypted_message(msg['content'], hashed_secret_key)} for msg in user['messages']]})
             elif CHANGE_TO_AUDIO_QUICK_REPLY in user_message and (LINE_REPLY == "Audio" or LINE_REPLY == "Both"):
+                print(f"{CHANGE_TO_AUDIO_QUICK_REPLY}")
                 exec_functions == True
                 audio_or_text = "Audio"
                 user['audio_or_text'] = audio_or_text
