8aa4f8606ded426bcc67e2797b93e4662f905b00
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-02-09 20:38:47 +0900

    Update functions_config.py

--- functions_config.py
+++ functions_config.py
@@ -3,7 +3,7 @@ functions = [
         # 関数の名称
         "name": "clock",
         # 関数の機能説明
-        "description": "useful for when you need to know what time it is.",
+        "description": "useful for when you need to know what time it is."
     },
     {
         # 関数の名称
