e4a3f125b69c8fb9f58e40f4a40ffa7d047dee57
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-03-06 23:06:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -480,37 +480,43 @@ def get_decrypted_message(enc_message, hashed_secret_key):
 
 @firestore.transactional
 def update_monthly_usage(transaction, doc_ref):
-    snapshot = doc_ref.get(transaction=transaction)
-    i_monthly_usage = 0
+    try:
+        snapshot = doc_ref.get(transaction=transaction)
+        monthly_usage = 0
 
-    if snapshot.exists:
-        last_updated = snapshot.get('last_updated')
-        print(f"last_updated: {last_updated}")
-        last_updated_date = last_updated.astimezone(jst).date() if last_updated else None
-        print(f"last_updated_date: {last_updated_date}")
-        # i_monthly_usage = snapshot.get('monthly_usage', 0)
-        i_monthly_usage = snapshot['monthly_usage']
-        print(f"i_monthly_usage: {i_monthly_usage}")
-        if last_updated_date is None or last_updated_date.month != nowDate.month:
-            i_monthly_usage = 1  # 新しい月になったら1にリセット
+        if snapshot.exists:
+            last_updated = snapshot.get('last_updated')
+            print(f"last_updated: {last_updated}")
+            last_updated_date = last_updated.astimezone(jst).date() if last_updated else None
+            print(f"last_updated_date: {last_updated_date}")
+            monthly_usage = snapshot.get('monthly_usage', 0)
+            print(f"monthly_usage: {monthly_usage}")
+            if last_updated_date is None or last_updated_date.month != nowDate.month:
+                monthly_usage = 1  # 新しい月になったら1にリセット
+            else:
+                monthly_usage += 1  # 同じ月ならインクリメント
+            print(f"monthly_usage: {monthly_usage}")
+            # ドキュメントを更新
+            transaction.update(doc_ref, {
+                'monthly_usage': monthly_usage,
+                'last_updated': nowDate
+            })
+            print(f"nowDate: {nowDate}")
         else:
-            i_monthly_usage += 1  # 同じ月ならインクリメント
-        print(f"i_monthly_usage: {i_monthly_usage}")
-        # ドキュメントを更新
-        transaction.update(doc_ref, {
-            'monthly_usage': i_monthly_usage,
-            'last_updated': nowDate
-        })
-        print(f"nowDate: {nowDate}")
-    else:
-        # ドキュメントが存在しない場合、新しいドキュメントを作成
-        i_monthly_usage = 1
-        transaction.set(doc_ref, {
-            'monthly_usage': i_monthly_usage,
-            'last_updated': nowDate
-        })
-
-    return i_monthly_usage
+            # ドキュメントが存在しない場合、新しいドキュメントを作成
+            monthly_usage = 1
+            transaction.set(doc_ref, {
+                'monthly_usage': monthly_usage,
+                'last_updated': nowDate
+            })
+    
+        return monthly_usage
+    except Exception as e:
+        print(f"Error: {e}")
+        bot_reply_list.append(['text', ERROR_MESSAGE + f": {e}"])
+        line_reply(reply_token, bot_reply_list)
+        raise
+    
 
 @app.route("/", methods=["POST"])
 def callback():
