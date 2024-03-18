a885186241cd8a5a65d5a1e63d58c338e9908a74
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-03-18 10:40:40 +0900

    Update localllm.py

--- localllm.py
+++ localllm.py
@@ -1,19 +1,29 @@
 import openai
 
 openai_api_key = os.getenv('LOCALLLM_API_KEY')
-gpt_client = OpenAI(api_key=openai_api_key)
-
-
-openai.api_key = "..."
-openai.api_base = "http://127.0.0.1:5000/v1"
 openai.api_version = "2023-05-15"
 
-def run_conversation(messages):
+def run_conversation(api_base, messages):
+    openai.api_base = api_base
     try:
-        response = gpt_client.chat.completions.create(
+        response = openai.chat.completions.create(
             messages=messages,
         )
         return response  # レスポンス全体を返す
     except Exception as e:
         print(f"An error occurred: {e}")
         return None  # エラー時には None を返す
+
+def localllm_functions(LOCALLLM_BASE_URL, messages_for_api):
+    public_img_url = None
+    public_img_url_s = None
+
+    i_messages_for_api = messages_for_api.copy()
+
+    response = run_conversation(api_base, i_messages_for_api)
+        if response:
+            bot_reply = response.choices[0].message.content
+        else:
+            bot_reply = "An error occurred while processing the question"
+        return bot_reply, public_img_url, public_img_url_s
+    
