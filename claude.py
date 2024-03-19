e84be4eb991392c5ff6b9427763237a35f027242
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-03-19 11:19:06 +0900

    Update claude.py

--- claude.py
+++ claude.py
@@ -318,7 +318,7 @@ def claude_functions(CLAUDE_MODEL, SYSTEM_PROMPT ,messages_for_api, USER_ID, mes
     print(f"response: {response}")
     bot_reply = response
     i_messages_for_api.append({'role': 'assistant', 'content': bot_reply})
-    i_messages_for_api.append({'role': 'user', 'content': 'SYSTEM:以上の結果を元に回答してください。')
+    i_messages_for_api.append({'role': 'user', 'content': 'SYSTEM:以上の結果を元に回答してください。'})
     response = run_conversation(CLAUDE_MODEL, SYSTEM_PROMPT, i_messages_for_api)
     print(f"response: {response}")
     bot_reply = response.content[0].text
