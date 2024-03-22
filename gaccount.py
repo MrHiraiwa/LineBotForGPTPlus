3d7389446169ceac1df62a1c38f84d3c01957465
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-03-22 10:54:18 +0900

    Update gaccount.py

--- gaccount.py
+++ gaccount.py
@@ -2,14 +2,12 @@ from google_auth_oauthlib.flow import Flow
 import os
 import google.auth.transport.requests
 
-REDIRECT_URI = 'http://localhost:5000/callback'
-
-def create_oauth_session():
+def create_oauth_session(GACCOUNT_CALLBACK_URL):
     # OAuth 2.0 クライアントフローを設定
     flow = Flow.from_client_secrets_file(
         'path/to/client_secrets.json',
         scopes=['openid', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'],
-        redirect_uri=REDIRECT_URI)
+        redirect_uri=GACCOUNT_CALLBACK_URL)
 
     authorization_url, state = flow.authorization_url(prompt='consent')
 
