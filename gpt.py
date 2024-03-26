64cb947e6727eca64d5322c52f347063b94cdae5
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-03-26 14:18:58 +0900

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -558,7 +558,7 @@ def chatgpt_functions(GPT_MODEL, messages_for_api, USER_ID, message_id, ERROR_ME
                 elif function_call.name == "update_calendar" and not update_calendar_called:
                     update_calendar_called = True
                     arguments = json.loads(function_call.arguments)
-                    bot_reply, gaccount_access_token, gaccount_refresh_token = update_calendar(gaccount_access_token, gaccount_refresh_token,arguments["event_id"], arguments["summary"], arguments["start_time"], arguments["end_time"], arguments["description"], arguments["location"])
+                    bot_reply, gaccount_access_token, gaccount_refresh_token = update_calendar(gaccount_access_token, gaccount_refresh_token, arguments["event_id"], arguments["summary"], arguments["start_time"], arguments["end_time"], arguments["description"], arguments["location"])
                     i_messages_for_api.append({"role": "assistant", "content": bot_reply})
                     attempt += 1
                 elif function_call.name == "delete_calendar" and not delete_calendar_called:
