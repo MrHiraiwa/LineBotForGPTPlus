be4d069d56a86b60960f1b37e94263d320c79d8c
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-03-26 13:42:41 +0900

    Update gpt_config.py

--- gpt_config.py
+++ gpt_config.py
@@ -99,7 +99,7 @@ functions = [
         # 関数の名称
         "name": "get_calendar",
         # 関数の機能説明
-        "description": "You can retrieve upcoming schedules."
+        "description": "You can retrieve upcoming schedules and the event ID of the schedule."
     },
     {
         # 関数の名称
@@ -140,8 +140,61 @@ functions = [
     },
     {
         # 関数の名称
-        "name": "get_gmail",
+        "name": "update_calendar",
         # 関数の機能説明
-        "description": "You can retrieve gmail message."
-    }    
+        "description": "You can update schedules by the event ID of the schedule.",
+        "parameters": {
+            "type": "object",
+            # 各引数
+            "properties": {
+                "event_id": {
+                    "type": "string",
+                    # 引数の説明
+                    "description": "スケジュールのイベントID"
+                },
+                "summary": {
+                    "type": "string",
+                    # 引数の説明
+                    "description": "更新後のスケジュールのサマリー"
+                },
+                "start_time": {
+                    "type": "string",
+                    # 引数の説明
+                    "description": "更新後のスケジュールの開始時間を日本時間で指定(RFC3339)"
+                },
+                "end_time": {
+                    "type": "string",
+                    # 引数の説明
+                    "description": "更新後のスケジュールの終了時間を日本時間で指定(RFC3339)"
+                },
+                "description": {
+                    "type": "string",
+                    # 引数の説明
+                    "description": "更新後のスケジュールした内容の詳細な説明"
+                },
+                "location": {
+                    "type": "string",
+                    # 引数の説明
+                    "description": "更新後のスケジュールの内容を実施する場所"
+                }
+            }
+        }
+    },
+    {
+        # 関数の名称
+        "name": "delete_calendar",
+        # 関数の機能説明
+        "description": "You can delete schedules by the event ID of the schedule.",
+        "parameters": {
+            "type": "object",
+            # 各引数
+            "properties": {
+                "event_id": {
+                    "type": "string",
+                    # 引数の説明
+                    "description": "スケジュールのイベントID"
+                }
+            }
+        }
+    }   
 ]
