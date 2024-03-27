2d897dda1b0aa5b6c16bec17202b6cf61316e747
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-03-27 12:11:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -530,7 +530,8 @@ def update_monthly_usage(transaction, doc_ref):
             last_updated = snapshot.get('last_updated')
             last_updated_date = last_updated.astimezone(jst).date() if last_updated else None
             monthly_usage = snapshot.get('monthly_usage') if snapshot.exists and 'monthly_usage' in snapshot.to_dict() else 0
-            monthly_free = snapshot.get('monthly_free', False)
+            monthly_free = snapshot.get('monthly_free') if 'monthly_free' in snapshot.to_dict() else False
+
             if last_updated_date is None or last_updated_date.month != nowDate.month:
                 monthly_usage = 1  # 新しい月になったら1にリセット
                 monthly_free = False # 新しい月になったら無料期間をリセット
@@ -549,6 +550,7 @@ def update_monthly_usage(transaction, doc_ref):
             monthly_usage = 1
             transaction.set(doc_ref, {
                 'monthly_usage': monthly_usage,
+                'monthly_free': monthly_free,
                 'last_updated': nowDate
             })
     
