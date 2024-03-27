33e9561ccac8bd4fd4d093c2b79e75fd2ef6bb0e
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-03-27 09:29:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -908,7 +908,7 @@ def handle_message(event):
                 head_message = head_message + NG_MESSAGE 
         
             if 'start_free_day' in user:
-                print(f"nowDATE: {nowDate.date()}, start_free_day: {start_free_day.date()).days}, FREE_LIMIT_DAY: {FREE_LIMIT_DAY}")
+                print(f"nowDATE: {nowDate.date()}, start_free_day: {start_free_day.date().days}, FREE_LIMIT_DAY: {FREE_LIMIT_DAY}")
                 if (nowDate.date() - start_free_day.date()).days < FREE_LIMIT_DAY:
                     dailyUsage = None
             if  source_type == "group" or source_type == "room":
