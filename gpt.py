3ded591abe394dc4f53eca2dc31f9862a448da58
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-04-05 09:31:04 +0900

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -505,6 +505,7 @@ def run_conversation_f(GPT_MODEL, FUNCTIONS, messages, google_description, custo
             functions=functions,
             function_call="auto",
         )
+        print(f"functions: {functions}")
         # downdate_function_descriptions(functions, google_description, "get_googlesearch")
         # downdate_function_descriptions(functions, custom_description, "get_customsearch1")
         return response  # レスポンス全体を返す
