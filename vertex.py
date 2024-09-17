98ef2d1b3ea693a66b0a461b85d3ca7f5a6afb1e
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-09-17 15:33:06 +0900

    Update vertex.py

--- vertex.py
+++ vertex.py
@@ -591,6 +591,13 @@ def run_conversation_f(VERTEX_MODEL, FUNCTIONS, messages, google_description, cu
         name="get_time",
         description="useful for when you need to know what time it is.",
         parameters={
+            "type": "object",
+            "properties": {
+                "dummy": {
+                    "type": "string",
+                    "description": "設定不要"
+                }
+            }
         },
     )
 
