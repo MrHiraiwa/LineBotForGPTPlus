8f4e0ac1685047379e11b7bbd087d636da051299
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-09-17 17:18:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -237,8 +237,8 @@ DEFAULT_ENV_VARS = {
     'CORE_AI_TYPE': 'GPT',
     'CLAUDE_MODEL': 'claude-3-haiku-20240307',
     'LOCALLLM_BASE_URL': 'https://127.0.0.1:5000/v1',
-    'VERTEX_MODEL': 'gemini-pro',
-    'PUT_VERTEX_MODEL': 'gemini-pro',
+    'VERTEX_MODEL': 'gemini-1.5-pro-001',
+    'PUT_VERTEX_MODEL': 'gemini-1.5-pro-001',
     'BLOCKED_ACCOUNT_MESSAGE': 'システム管理者によりアカウントがブロックされました。',
     'BLOCKED_NEW_ACCOUNTS': 'False',
     'BLOCKED_NEW_ACCOUNTS_MESSAGE': '現在、新規利用者の登録を停止しています。'
