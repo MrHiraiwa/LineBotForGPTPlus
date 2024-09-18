4d1ad116922afbf601c79c7c2e367a7f96bd7b07
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-09-18 19:36:14 +0900

    Update vertex.py

--- vertex.py
+++ vertex.py
@@ -953,7 +953,7 @@ def run_conversation_f(VERTEX_MODEL, system_instruction, FUNCTIONS, messages, go
         print(f"An error occurred: {e}")
         return None  # エラー時には None を返す
 
-def vertex_functions(VERTEX_MODEL, FUNCTIONS, messages_for_api, USER_ID, message_id, ERROR_MESSAGE, PAINT_PROMPT, BUCKET_NAME, FILE_AGE, GOOGLE_DESCRIPTION, CUSTOM_DESCRIPTION, gaccount_access_token, gaccount_refresh_token, CORE_IMAGE_TYPE="", max_attempts=5):
+def vertex_functions(VERTEX_MODEL, FUNCTIONS, messages_for_api, USER_ID, message_id, ERROR_MESSAGE, PAINT_PROMPT, BUCKET_NAME, FILE_AGE, GOOGLE_DESCRIPTION, CUSTOM_DESCRIPTION, gaccount_access_token, gaccount_refresh_token, CORE_IMAGE_TYPE="", VERTEX_IMAGE_MODEL="", max_attempts=5):
     public_img_url = None
     public_img_url_s = None
     user_id = USER_ID
