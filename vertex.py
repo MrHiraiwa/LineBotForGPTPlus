8d8f49bc02fc6bd7560c2aecf8bbc71305d0a84d
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-09-18 15:36:20 +0900

    Update vertex.py

--- vertex.py
+++ vertex.py
@@ -771,7 +771,7 @@ def run_conversation_f(VERTEX_MODEL, system_instruction, FUNCTIONS, messages, go
         response = model.generate_content(
             messages,
             generation_config={"temperature": 0},
-            tools= [get_time_tool],
+            tools= functions,
         )
         return response  # レスポンス全体を返す
     except Exception as e:
